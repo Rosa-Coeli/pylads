bd36f70b8ce104877cc022ed038a87ab564925b2
Jakub Kusnier <kusnier.j@seznam.cz>
2019-11-26 22:10:18 +0100

    Creating Caesar cipher programm.
    Fuction main() implemented.

---
+++ 05/Kaesar_cipher.py
@@ -0,0 +1,14 @@
+def change_computing(change):
+
+
+
+    def ciphering(text, function):
+
+
+
+def main():
+    print("Hi, I will cipher your text with Caesar cipher")
+    text = input("Insert text you want to cipher: ")
+    change = input("Specify number: ")
+    function = change_computing(change)
+    return ciphering(text, function)
