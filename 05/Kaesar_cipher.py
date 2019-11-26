f40d6854e293bd74cc0cc993d72ce93cc2167eb7
Jakub Kusnier <kusnier.j@seznam.cz>
2019-11-26 23:01:36 +0100

    Implementation of change function creation.
    Function is a dictionary that hold what each character changes to.

--- 05/Kaesar_cipher.py
+++ 05/Kaesar_cipher.py
@@ -1,14 +1,23 @@
 def change_computing(change):
+    function = {}
+    for i in 'A' 'a':
+        order = ord(i)
+        for j in range(order, order+26):
+            function[chr(j)] = chr((j - order + change) % 26 + order)
+    return function
 
 
-
-    def ciphering(text, function):
-
+def ciphering(text, function):
+    pass
 
 
 def main():
     print("Hi, I will cipher your text with Caesar cipher")
     text = input("Insert text you want to cipher: ")
-    change = input("Specify number: ")
+    change = int(input("Specify number: "))
     function = change_computing(change)
-    return ciphering(text, function)
+    #return ciphering(text, function)
+    print(function)
+
+
+main()
