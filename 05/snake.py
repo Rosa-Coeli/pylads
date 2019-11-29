932635c2966b9b4097de79cdbe415754ea502826
Jakub <kusnier.j@seznam.cz>
2019-11-29 22:32:26 +0100

    Working on Snake.
    Implemented user input for starting corner and direction of snake.

--- 05/snake.py
+++ 05/snake.py
@@ -1,6 +1,57 @@
 from random import randint, choice
 
 
+def direction_f(starting_corner):
+    while True:
+        direction = input("""Set direction of the snake.\nFor clockwise type 'cw'.
+For counter-clockwise type 'cc'.\n""")
+        if direction == "cw":
+            if starting_corner == '0':
+                print(direction)
+                return (0, 1)
+            elif starting_corner == '1':
+                return (-1, 0)
+            elif starting_corner == '2':
+                return (0, -1)
+            elif starting_corner == '3':
+                return (1, 0)
+        elif direction == "cc":
+            if starting_corner == '0':
+                return (1, 0)
+            elif starting_corner == '1':
+                return (0, 1)
+            elif starting_corner == '2':
+                return (-1, 0)
+            elif starting_corner == '3':
+                return (0, -1)
+        else:
+            print('Error: Invalid input.\nTry again:\n')
+
+
+def starting_corner_set():
+    while True:
+        starting_corner = input("""Chose starting corner:
+For upper left corner write 0.
+For bottom left corner write 1.
+For upper right corner write 2.
+For bottom right corner write 3.\n""")
+        if starting_corner == "0" or starting_corner == "1" or starting_corner == "2" or starting_corner == "3":
+            return starting_corner
+        else:
+            print('Error: Wrong input value.\nChose again:\n')
+
+
+def starting_corner_position(starting_corner, n):
+    if starting_corner == 0:
+        return (0, 0)
+    elif starting_corner == 1:
+        return (n, 0)
+    elif starting_corner == 2:
+        return (0, n)
+    elif starting_corner == 3:
+        return (n, n)
+
+
 def random_char(last_char):
     if randint(0, 2) == 2:
         char_list = [chr(x) for x in range(ord("q"), ord("z")+1)]
@@ -21,15 +72,14 @@ def rounded_text(text, text_modulo):
 
 
 def main():
-    start_corner = input("""Chose starting corner:
-    -For upper left corner write 0.
-    -For bottom left corner write 1.
-    -For upper right corner write 2.
-    -For bottom right corner write 3.""")
+    starting_corner = starting_corner_set()
+    print(starting_corner)
+    direction = direction_f(starting_corner)
     text = input('Insert the text you want to cipher:\n')
     text_modulo = len(text) % 4
     if  text_modulo != 0:
         text = rounded_text(text, 4-text_modulo)
+    starting_corner = starting_corner_position(starting_corner, 4)
 
     print(text)
 
