0af87b6c77b5323073fe11de7267c98d64e60636
Jakub Kusnier <kusnier.j@seznam.cz>
2019-12-09 01:22:35 +0100

    Now works when executed from any directory.

--- Roman_conversion.sh
+++ Roman_conversion.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
+dir_name=$(dirname $(realpath $0))
+
 echo Insert integer in either roman or arabic numerals to get the other.
 while :
 do
 	read input
 	if [[ $input =~ ^[0-9]+$ ]]; then
-		py ./05/Arabic_numerals_to_Roman.py $input
+		py $dir_name/05/Arabic_numerals_to_Roman.py $input
 	elif [[ $input =~ ^-n[[:blank:]]+[MDCLXVImdclxvi]+$ ]]; then
 		input=${input:3}
-		arabic=$(py ./05/Roman_numerals_to_Arabic.py ${input^^})
-		roman=$(py ./05/Arabic_numerals_to_Roman.py $arabic)
+		arabic=$(py $dir_name/05/Roman_numerals_to_Arabic.py ${input^^})
+		roman=$(py $dir_name/05/Arabic_numerals_to_Roman.py $arabic)
 		echo Normalized roman numeral is probably $roman which equals $arabic.
-	elif [[ $input =~ ^[IVXLCDM]+$ ]]; then
-		py ./05/Roman_numerals_to_Arabic.py $input
+	elif [[ $input =~ ^[IVXLCDMivxlcdm]+$ ]]; then
+		py $dir_name/05/Roman_numerals_to_Arabic.py ${input^^}
+	elif [[ $input =~ exit|quit ]]; then
+		break
 	else	
 		echo Just no.
 	fi
