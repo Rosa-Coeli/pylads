ca896fb6e0c7a7325b37885de9ed017ceb7fe564
Jakub Kusnier <kusnier.j@seznam.cz>
2019-12-13 00:33:47 +0100

    Guitar Hero design demo

---
+++ guitar_hero/guitar_hero.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+clear
+sleep 3
+
+while :
+do
+	echo '	C	1  | | | | O |'
+	echo '		   ___________'
+	echo '		   | | O | | |'
+	echo '		   ___________'
+	echo '		3  | O | | | |'
+	echo '	       	   ___________'
+	echo
+	echo '		       ---    '
+	echo
+	sleep 2
+	echo '	G	2  | O | | | |'
+	echo '		   ___________'
+	echo '		3  O | | | | O'
+	echo '		   ___________'
+	echo
+	echo '		       ---    '
+	echo
+	sleep 2
+done
