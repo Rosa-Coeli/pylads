bd00b84718318465de76dce290b8c30c1aa1c1f1
Jakub Kusnier <kusnier.j@seznam.cz>
2019-12-23 09:48:46 +0100

    Revert "Revert "Guess the picture added.""
    This reverts commit 02434653ef0468cdcbbcddc598c5075f8f4fe38d.

---
+++ guess_teh_picture/menu_guess.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+dir_name=$(dirname $(realpath $0))
+
+echo fraction: 
+read fraction
+
+for i in *.[Jj][Pp][Gg];
+do
+	py guess_the_picture.py $i $fraction
+	read a
+	echo $a
+	read a
+	echo $a
+done
