a684394ac7e7c4b0e6cf97515db9d8a473916a36
Jakub Kusnier <kusnier.j@seznam.cz>
2019-12-28 16:26:33 +0100

    Guitar_hero.sh is now menu script.

--- guitar_hero/guitar_hero.sh
+++ guitar_hero/guitar_hero.sh
@@ -1,26 +1,49 @@
 #!/bin/bash
 
+
+dir_path=$(dirname $(realpath $0))
+
+
+echo -n Write how many seconds of time you want inbetween chords: 
+read wait_time
+
+
+gap () {
+	echo
+	echo '		       ---    '
+	echo
+	sleep $wait_time
+}
+
+
+commands () {
+	echo Welcome to the Guitar Realo.
+	echo Type:
+	echo -\'exit\', \'quit\' or \'x\' to exit.
+	echo -\'help\' to show commands.
+	echo -\'insert\' to insert new chords.
+	echo -\'play\' to start playing!
+}
+
+
+
 clear
 sleep 3
 
 while :
 do
-	echo '	C	1  | | | | O |'
-	echo '		   ___________'
-	echo '		   | | O | | |'
-	echo '		   ___________'
-	echo '		3  | O | | | |'
-	echo '	       	   ___________'
-	echo
-	echo '		       ---    '
-	echo
-	sleep 2
-	echo '	G	2  | O | | | |'
-	echo '		   ___________'
-	echo '		3  O | | | | O'
-	echo '		   ___________'
-	echo
-	echo '		       ---    '
-	echo
-	sleep 2
+	read input
+	case $input in 
+		exit | quit | x)
+			break
+			;;
+		help)
+			commands
+			;;
+		insert)
+			./user_input.sh
+			;;
+		play)
+			;;
+	esac
 done
